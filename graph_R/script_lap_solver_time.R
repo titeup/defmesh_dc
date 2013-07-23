7f549a5683f88d02a47fc5e6856cecd56e6c6876
lthebault <loic.thebault@prism.uvsq.fr>
2013-07-23 18:22:04 +0200

    remove line points

--- graph_R/script_lap_solver_time.R
+++ graph_R/script_lap_solver_time.R
@@ -18,7 +18,7 @@ p <- p + labs(x="Cores", y="Seconds")
 p <- p + scale_colour_manual(name="", values = c("black"), labels=c("Ideal Scaling"))
 p <- p + scale_fill_manual(name="", values=c("#99CCFF","#338888"), labels=c("Ref", "D&C"))
 p <- p + geom_line(data=tab_ideal, aes(x=as.factor(Cores), y=value, group=1, color=variable), size=1)
-p <- p + geom_point(data=tab_ideal, aes(x=as.factor(Cores), y=value, group=1, color=variable), size=2.2)
+#p <- p + geom_point(data=tab_ideal, aes(x=as.factor(Cores), y=value, group=1, color=variable), size=2.2)
 p <- p + theme_bw() + theme(legend.key = element_blank())
 
 pdf(paste(f,".pdf",sep=""), width=5, height=3)
