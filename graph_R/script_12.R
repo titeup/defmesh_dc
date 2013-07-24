2f8b3218a7e065850ff81916c9f51b00a0c7143c
lthebault <loic.thebault@prism.uvsq.fr>
2013-07-24 18:53:41 +0200

    Update figures & add 12 cores comparison

---
+++ graph_R/script_12.R
@@ -0,0 +1,21 @@
+#!/usr/bin/env Rscript
+
+require(ggplot2)
+require(reshape2)
+
+f="Elasticity_asm_time_12_cores"
+
+tab = read.table(file=paste(f,".csv",sep=""), sep=",", header=TRUE)
+tab$Version=reorder(tab$Version,tab$Time)
+tab$Time=as.double(tab$Time/(10**9))
+
+p <- ggplot(tab, aes(x=as.factor(Version), y=Time))
+p <- p + geom_bar(position=position_dodge(),stat="identity", width=0.5)
+p <- p + labs(x="MPI*Threads (Version)", y="Giga Cycles")
+p <- p + theme_bw() + theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1))
+p <- p + geom_line(aes(x=as.factor(Version), y=3.61, group=1, color="red"))
+p <- p + theme(legend.position="none")
+
+pdf(paste(f,".pdf",sep=""), width=4, height=3)
+print(p)
+dev.off()
--- graph_R/script_elas_asm_time.R
+++ graph_R/script_elas_asm_time.R
@@ -10,19 +10,17 @@ tab.m <- melt(tab, id.vars="Cores")
 tab_all=tab.m[tab.m$variable != "Ideal.scaling",]
 tab_ideal=tab.m[tab.m$variable == "Ideal.scaling",]
 tab_all$variable = factor(tab_all$variable)
-
-tab_all$value = as.double(tab_all$value/(10**10))
-tab_ideal$value = as.double(tab_ideal$value/(10**10))
+tab_all$value = as.double(tab_all$value/(10**9))
+tab_ideal$value = as.double(tab_ideal$value/(10**9))
 
 p <- ggplot(tab_all, aes(x=as.factor(Cores), y=value))
 p <- p + geom_bar(aes(fill=variable),position=position_dodge(), stat="identity")
 p <- p + scale_x_discrete(breaks=c(1,2,4,6,8,12))
-p <- p + labs(x="Cores", y="Cycles (10e+10)")
+p <- p + labs(x="Cores", y="Giga Cycles")
 p <- p + scale_colour_manual(name="", values=c("black"), labels=c("Ideal Scaling"))
 p <- p + scale_fill_manual(name="", values=c("#99CCFF","#338888","#005555"),
 						   labels=c("Ref", "D&C", "Coloring"))
 p <- p + geom_line(data=tab_ideal, aes(x=as.factor(Cores), y=value, group=1, color=variable), size=1)
-#p <- p + geom_point(data=tab_ideal, aes(x=as.factor(Cores), y=value, group=1, color=variable), size=2.2)
 p <- p + theme_bw() + theme(legend.position="none")
 
 pdf(paste(f,".pdf",sep=""), width=4, height=2.5)
