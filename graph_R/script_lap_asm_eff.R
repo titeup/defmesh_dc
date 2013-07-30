607d00702e9d98eadeb6c11555a5051410591e85
lthebault <loic.thebault@prism.uvsq.fr>
2013-07-30 18:30:41 +0200

    Finalize figures

--- graph_R/script_lap_asm_eff.R
+++ graph_R/script_lap_asm_eff.R
@@ -7,11 +7,14 @@ f="Laplacian_asm_efficiency"
 
 tab = read.table(file=paste(f,".csv",sep=""), sep=",", header=TRUE)
 tab.m <- melt(tab, id.vars="Cores")
+tab_all=tab.m[tab.m$variable != "Ideal.scaling",]
+tab_ideal=tab.m[tab.m$variable == "Ideal.scaling",]
 
-p <- ggplot(tab.m, aes(x=as.factor(Cores), y=value))
-p <- p + geom_line(aes(group=variable, color=variable), size=1)
-p <- p + scale_colour_manual(values=c("black","#99CCFF","#338888","#005555"),name="",
-							 labels=c("Ideal Scaling","Ref", "D&C", "Coloring"))
+p <- ggplot(tab_all, aes(x=as.factor(Cores), y=value))
+p <- p + geom_line(data=tab_all, aes(x=as.factor(Cores), y=value, group=variable, color=variable, size=variable))
+p <- p + geom_line(data=tab_ideal, aes(x=as.factor(Cores), y=value, group=1, color=variable, size=variable))
+p <- p + scale_color_manual(name="", values=c("black", "#99CCFF","#338888","#005555"), labels=c("Ideal Scaling", "Ref", "D&C", "Coloring"))
+p <- p + scale_size_manual(name="", values=c(0.4,1.2,1.2,1.2), labels=c("Ideal Scaling", "Ref", "D&C", "Coloring"))
 p <- p + scale_x_discrete(breaks=c(1,2,4,6,8,12))
 p <- p + labs(x="Cores", y="Efficiency")
 p <- p + theme_bw() + theme(legend.key = element_blank())
--- graph_R/script_lap_solver_time.R
+++ graph_R/script_lap_solver_time.R
@@ -9,16 +9,17 @@ tab = read.table(file=paste(f,".csv",sep=""), sep=",", header=TRUE)
 tab.m <- melt(tab, id.vars="Cores")
 tab_all=tab.m[tab.m$variable != "Ideal.scaling",]
 tab_ideal=tab.m[tab.m$variable == "Ideal.scaling",]
-tab_all$variable = factor(tab_all$variable)
 
 p <- ggplot(tab_all, aes(x=as.factor(Cores), y=value))
-p <- p + geom_bar(aes(fill=variable),position=position_dodge(), stat="identity")
+p <- p + geom_bar(data=tab_all, aes(fill=variable), position=position_dodge(), stat="identity", show_guide=FALSE)
+p <- p + scale_fill_manual(values=c("#99CCFF","#338888"))
+p <- p + geom_line(data=tab_all, aes(x=as.factor(Cores), y=value, group=NULL, color=variable, size=variable))
+p <- p + geom_line(data=tab_ideal, aes(x=as.factor(Cores), y=value, group=1, color=variable, size=variable))
+p <- p + scale_color_manual(name="", values=c("black","#99CCFF","#338888"), labels=c("Ideal Scaling","Ref","D&C"))
+p <- p + scale_size_manual(name="", values=c(0.5,1.2,1.2), labels=c("Ideal Scaling","Ref","D&C"))
 p <- p + scale_x_discrete(breaks=c(1,2,4,6,8,12))
 p <- p + labs(x="Cores", y="Seconds")
-p <- p + scale_colour_manual(name="", values = c("black"), labels=c("Ideal Scaling"))
-p <- p + scale_fill_manual(name="", values=c("#99CCFF","#338888"), labels=c("Ref", "D&C"))
-p <- p + geom_line(data=tab_ideal, aes(x=as.factor(Cores), y=value, group=1, color=variable), size=1)
-p <- p + theme_bw() + theme(legend.key = element_blank())
+p <- p + theme_bw() + theme(legend.key=element_blank())
 
 pdf(paste(f,".pdf",sep=""), width=5.1, height=2.35)
 print(p)
